5cf2cf91d4d5d89d12e97cf0d378480ea0a600b7
Ji O <kimjioh0927@gmail.com>
2023-03-07 01:27:11 +0900

    Change to reference the submodule's httplib instead of the local httplib file

--- module/src/main/cpp/notifier/notifier.cpp
+++ module/src/main/cpp/notifier/notifier.cpp
@@ -1,41 +1,36 @@
 #include <string>
-#include "httplib.h"
+#include <httplib.h>
 #include "../stdinclude.hpp"
 
-namespace notifier
-{
-    httplib::Client* client = nullptr;
+namespace notifier {
+    httplib::Client *client = nullptr;
 
-    void init()
-    {
+    void init() {
         client = new httplib::Client(g_packet_notifier);
         client->set_connection_timeout(0, 50000);
     }
 
-    void notify_response(const std::string& data)
-    {
+    void notify_response(const std::string &data) {
         if (client == nullptr) {
             init();
         }
 
         auto res = client->Post("/notify/response", data, "application/x-msgpack");
-        httplib::Error error = res.error();
-        if (error != httplib::Error::Success)
-        {
-            LOGE("Unexpected error from notifier: %s\n",httplib::to_string(error).c_str());
+        const auto error = res.error();
+        if (error != httplib::Error::Success) {
+            LOGE("Unexpected error from notifier: %s\n", httplib::to_string(error).c_str());
         }
     }
-    void notify_request(const std::string& data)
-    {
+
+    void notify_request(const std::string &data) {
         if (client == nullptr) {
             init();
         }
 
         auto res = client->Post("/notify/request", data, "application/x-msgpack");
-        httplib::Error error = res.error();
-        if (error != httplib::Error::Success)
-        {
-            LOGE("Unexpected error from notifier: %s\n",httplib::to_string(error).c_str());
+        const auto error = res.error();
+        if (error != httplib::Error::Success) {
+            LOGE("Unexpected error from notifier: %s\n", httplib::to_string(error).c_str());
         }
     }
 }
