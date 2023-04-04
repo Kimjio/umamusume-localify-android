aa3644096fccfd0b317cbd85532cb2e2dd7eee13
Ji O <kimjioh0927@gmail.com>
2023-04-05 00:17:40 +0900

    Modify scripts to copy library files instead of bind library files (#18)

--- template/magisk_module/post-fs-data.sh
+++ template/magisk_module/post-fs-data.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-while read line; do echo ${line} | grep jp.co.cygames.umamusume | awk '{print $2}' | xargs umount -l; done< /proc/mounts
-while read line; do echo ${line} | grep com.kakaogames.umamusume | awk '{print $2}' | xargs umount -l; done< /proc/mounts
-while read line; do echo ${line} | grep com.komoe.kmumamusumegp | awk '{print $2}' | xargs umount -l; done< /proc/mounts
-while read line; do echo ${line} | grep com.komoe.kmumamusumemc | awk '{print $2}' | xargs umount -l; done< /proc/mounts
+while read -r line; do echo "${line}" | grep jp.co.cygames.umamusume | awk '{print $2}' | xargs umount -l; done </proc/mounts
+while read -r line; do echo "${line}" | grep com.kakaogames.umamusume | awk '{print $2}' | xargs umount -l; done </proc/mounts
+while read -r line; do echo "${line}" | grep com.komoe.kmumamusumegp | awk '{print $2}' | xargs umount -l; done </proc/mounts
+while read -r line; do echo "${line}" | grep com.komoe.kmumamusumemc | awk '{print $2}' | xargs umount -l; done </proc/mounts
--- template/magisk_module/service.sh
+++ template/magisk_module/service.sh
@@ -10,46 +10,45 @@ MODULE_LIB_NAME=@MODULE_LIB_NAME@
 
 ABI=$(getprop ro.product.cpu.abi)
 
-if [ $ABI = "x86" ]; then
+copy_lib() {
+  PACKAGE=$1
+  PID=$(dumpsys package "$PACKAGE" | grep userId= | sed 's/[[:blank:]]*userId=//g')
+  cp "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/"$PACKAGE"/armeabi-v7a.so
+  chown "$PID":"$PID" /data/data/"$PACKAGE"/armeabi-v7a.so
+}
+
+copy_lib64() {
+  PACKAGE=$1
+  PID=$(dumpsys package "$PACKAGE" | grep userId= | sed 's/[[:blank:]]*userId=//g')
+  cp "/data/adb/modules/${API}_${MODULE_ID}/lib/arm64-v8a/lib${MODULE_LIB_NAME}.so" /data/data/"$PACKAGE"/arm64-v8a.so
+  chown "$PID":"$PID" /data/data/"$PACKAGE"/arm64-v8a.so
+  copy_lib "$PACKAGE"
+}
+
+if [ "$ABI" = "x86" ]; then
   if [ -d "/data/data/jp.co.cygames.umamusume" ]; then
-    touch /data/data/jp.co.cygames.umamusume/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/jp.co.cygames.umamusume/armeabi-v7a.so
+    copy_lib "jp.co.cygames.umamusume"
   fi
   if [ -d "/data/data/com.kakaogames.umamusume" ]; then
-    touch /data/data/com.kakaogames.umamusume/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/com.kakaogames.umamusume/armeabi-v7a.so
+    copy_lib "com.kakaogames.umamusume"
   fi
   if [ -d "/data/data/com.komoe.kmumamusumegp" ]; then
-    touch /data/data/com.komoe.kmumamusumegp/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/com.komoe.kmumamusumegp/armeabi-v7a.so
+    copy_lib "com.komoe.kmumamusumegp"
   fi
   if [ -d "/data/data/com.komoe.kmumamusumemc" ]; then
-    touch /data/data/com.komoe.kmumamusumemc/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/com.komoe.kmumamusumemc/armeabi-v7a.so
+    copy_lib "com.komoe.kmumamusumemc"
   fi
 elif [ $ABI = "x86_64" ]; then
   if [ -d "/data/data/jp.co.cygames.umamusume" ]; then
-    touch /data/data/jp.co.cygames.umamusume/arm64-v8a.so
-    touch /data/data/jp.co.cygames.umamusume/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/arm64-v8a/lib${MODULE_LIB_NAME}.so" /data/data/jp.co.cygames.umamusume/arm64-v8a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/jp.co.cygames.umamusume/armeabi-v7a.so
+    copy_lib64 "jp.co.cygames.umamusume"
   fi
   if [ -d "/data/data/com.kakaogames.umamusume" ]; then
-    touch /data/data/com.kakaogames.umamusume/arm64-v8a.so
-    touch /data/data/com.kakaogames.umamusume/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/arm64-v8a/lib${MODULE_LIB_NAME}.so" /data/data/com.kakaogames.umamusume/arm64-v8a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/com.kakaogames.umamusume/armeabi-v7a.so
+    copy_lib64 "com.kakaogames.umamusume"
   fi
   if [ -d "/data/data/com.komoe.kmumamusumegp" ]; then
-    touch /data/data/com.komoe.kmumamusumegp/arm64-v8a.so
-    touch /data/data/com.komoe.kmumamusumegp/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/arm64-v8a/lib${MODULE_LIB_NAME}.so" /data/data/com.komoe.kmumamusumegp/arm64-v8a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/com.komoe.kmumamusumegp/armeabi-v7a.so
+    copy_lib64 "com.komoe.kmumamusumegp"
   fi
   if [ -d "/data/data/com.komoe.kmumamusumemc" ]; then
-    touch /data/data/com.komoe.kmumamusumemc/arm64-v8a.so
-    touch /data/data/com.komoe.kmumamusumemc/armeabi-v7a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/arm64-v8a/lib${MODULE_LIB_NAME}.so" /data/data/com.komoe.kmumamusumemc/arm64-v8a.so
-    mount -o bind "/data/adb/modules/${API}_${MODULE_ID}/lib/armeabi-v7a/lib${MODULE_LIB_NAME}.so" /data/data/com.komoe.kmumamusumemc/armeabi-v7a.so
+    copy_lib64 "com.komoe.kmumamusumegp"
   fi
 fi
