b122f626bfaaafad3845c16cc310f1f15828306b
Ji O <kimjioh0927@gmail.com>
2023-04-06 03:07:00 +0900

    Temp: Add priconnegrandmasters on queries

--- app/src/main/java/com/kimjio/umamusumelocalify/settings/Constants.java
+++ app/src/main/java/com/kimjio/umamusumelocalify/settings/Constants.java
@@ -6,12 +6,14 @@ public final class Constants {
     public static final String PREF_NAME = "pref_settings";
 
     public static final String PKG_JAP = "jp.co.cygames.umamusume";
+    public static final String PKG_JAP_PCR_GM = "jp.co.cygames.priconnegrandmasters";
     public static final String PKG_KOR = "com.kakaogames.umamusume";
     public static final String PKG_TWN_GOOGLE = "com.komoe.kmumamusumegp";
     public static final String PKG_TWN_MY_CARD = "com.komoe.kmumamusumemc";
 
     public static final List<String> targetPackageNames = List.of(
             PKG_JAP,
+            PKG_JAP_PCR_GM,
             PKG_KOR,
             PKG_TWN_GOOGLE,
             PKG_TWN_MY_CARD
